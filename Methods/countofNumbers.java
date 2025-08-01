86e2524c5e050ca9e408943dcb263df6c11b3fa4
Ram kiran Randhi <randhiramkiran@gmail.com>
2025-08-01 22:48:15 +0530

    Add files via upload

---
+++ Methods/countofNumbers.java
@@ -0,0 +1,39 @@
+// // WRITE A PROGRAM TO  ENTER THE NUMBERS TILL USER WANTS AND AT THE END IT SHOULD DISPLAY THE COUNT OF POSITIVE , NEGATIVE,ZEROES.
+
+// import java.util.*;
+// public class countofNumbers {
+
+//     public static void main(String[] args) {
+//         int positive=0;
+//         int negative=0;
+//         int zeroes=0;
+
+//         System.out.print("Press 1 to Continue or  press 0 to Stop :");
+//         Scanner sc=new Scanner(System.in);
+//         int input=sc.nextInt();
+
+//         while(input==1){
+//             System.out.print("Enter the number :");
+//             int number=sc.nextInt();
+
+//             if(number>0){
+//                 positive++;
+//             }
+//             else if(number<0){
+//                 negative++;
+
+//             }
+//             else{
+//                 zeroes++;
+//             }
+//             System.out.print("Press 1 to  Continue or Press 0 to Stop :");
+//             input=sc.nextInt();
+
+//         }
+//         System.out.println("Positive is :"+ positive);
+//         System.out.println("Negative is :"+ negative);
+//         System.out.println("Zeroes is :"+ zeroes);
+
+
+//     }
+// }
